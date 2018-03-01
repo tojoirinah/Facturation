46bed5394dc22722151786a9a34107d866109181
DESKTOP-JL96664\ACER <tojoirinah@gmail.com>
2018-03-02 00:27:21 +0100

    commit

---
+++ Facturation/Facturation.Administration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Facturation.Administration.Startup))]
+namespace Facturation.Administration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

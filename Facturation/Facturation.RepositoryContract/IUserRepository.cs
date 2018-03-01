392f6ee4b09c450d4c96595471a94b517590921f
DESKTOP-JL96664\ACER <tojoirinah@gmail.com>
2018-03-01 23:01:06 +0100

    modif

--- Facturation/Facturation.RepositoryContract/IUserRepository.cs
+++ Facturation/Facturation.RepositoryContract/IUserRepository.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Facturation.RepositoryContract
+﻿namespace Facturation.RepositoryContract
 {
     public interface IUserRepository
     {
-        Func<> 
     }
 }
--- Facturation/Facturation.RepositoryHelper/Base/Repository.cs
+++ Facturation/Facturation.RepositoryHelper/Base/Repository.cs
@@ -1,12 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using NHibernate.Linq;
-using System;
-using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace Facturation.RepositoryHelper.Base
@@ -25,86 +18,32 @@ namespace Facturation.RepositoryHelper.Base
     {
         public IList<TEntity> GetAll(params Expression<Func<TEntity, object>>[] navigationProperties)
         {
-            List<TEntity> list;
-            using (var session = NhibernateSession.OpenSession())
-            {
-                var dbQuery = session.Query<TEntity>();
-
-                //Apply eager loading
-                dbQuery = navigationProperties.Aggregate(dbQuery, (current, navigationProperty) => current.Fetch<TEntity, object>(navigationProperty));
-
-                list = dbQuery.ToList<TEntity>();
-            }
+            List<TEntity> list = null;
             return list;
         }
 
         public IList<TEntity> GetList(Func<TEntity, bool> @where, params Expression<Func<TEntity, object>>[] navigationProperties)
         {
-            List<TEntity> list;
-            using (var session = NhibernateSession.OpenSession())
-            {
-                var dbQuery = session.Query<TEntity>();
-                dbQuery = navigationProperties.Aggregate(dbQuery, (current, navigationProperty) => current.Fetch<TEntity, object>(navigationProperty));
-                list = dbQuery.Where(where).ToList<TEntity>();
-            }
+            List<TEntity> list = null; 
             return list;
         }
 
         public TEntity GetSingle(Func<TEntity, bool> @where, params Expression<Func<TEntity, object>>[] navigationProperties)
         {
-            TEntity item;
-            using (var session = NhibernateSession.OpenSession())
-            {
-                var dbQuery = session.Query<TEntity>();
-                dbQuery = navigationProperties.Aggregate(dbQuery, (current, navigationProperty) => current.Fetch<TEntity, object>(navigationProperty));
-                item = dbQuery.FirstOrDefault(where);
-            }
+            TEntity item = null;
             return item;
         }
 
         public void Add(params TEntity[] items)
         {
-            using (var session = NhibernateSession.OpenSession())
-            {
-                using (var transaction = session.BeginTransaction())   //  Begin a transaction
-                {
-                    foreach (var item in items)
-                    {
-                        session.Save(item);
-                    }
-                    transaction.Commit();   //  Commit the changes to the database
-                }
-            }
         }
 
         public void Update(params TEntity[] items)
         {
-            using (var session = NhibernateSession.OpenSession())
-            {
-                using (var transaction = session.BeginTransaction())   //  Begin a transaction
-                {
-                    foreach (var item in items)
-                    {
-                        session.SaveOrUpdate(item);
-                    }
-                    transaction.Commit();   //  Commit the changes to the database
-                }
-            }
         }
 
         public void Remove(params TEntity[] items)
         {
-            using (var session = NhibernateSession.OpenSession())
-            {
-                using (var transaction = session.BeginTransaction())   //  Begin a transaction
-                {
-                    foreach (var item in items)
-                    {
-                        session.Delete(item);
-                    }
-                    transaction.Commit();   //  Commit the changes to the database
-                }
-            }
         }
     }
 }
